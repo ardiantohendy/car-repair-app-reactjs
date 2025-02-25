055a019cdaa5510d32e7c15b89913004da2e6d10
Ardiantohendy <bayuardianto421@gmail.com>
2025-02-25 23:21:22 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">DOCTOR CAR</div>
+      <div className="list">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Book</a>
+          </li>
+          <li>
+            <a href="#">Login/Register</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,45 @@
+import Header from "./Header";
+
+const MainPage = () => {
+  return (
+    <>
+      <div className="homePage">
+        <Header />
+        <div className="content">
+          <div className="mid">
+            <div className="text">
+              <h1>
+                FIND THE BEST <br />
+                SERVICE FOR YOUR CAR
+              </h1>
+              <p className="redOne">We Serve Better Than Any Other</p>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto aperiam exercitationem facilis.</p>
+            </div>
+            <div className="btn">
+              <button>Book Now</button>
+              <button>Learn More</button>
+            </div>
+          </div>
+          <div className="bot">
+            {/* <div className="listBot">
+              <div className="first">
+                <h2>Car Cleaning & Washing</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente voluptas illo esse nulla rem?</p>
+              </div>
+              <div className="second">
+                <h2>Maintenance & Security Check</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente voluptas illo esse nulla rem?</p>
+              </div>
+              <div className="third">
+                <h2>Repair Service & Inspections</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente voluptas illo esse nulla rem?</p>
+              </div>
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
