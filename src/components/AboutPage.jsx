92afa79729de0a250fd0a6a802bf557655545e12
Ardiantohendy <bayuardianto421@gmail.com>
2025-02-26 18:39:31 +0700

    new commit

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,12 +1,16 @@
+import topGarage1 from "../assets/image/topgarage1.jpg";
+import topGarage2 from "../assets/image/topgarage2.jpg";
+import topGarage3 from "../assets/image/topgarage3.jpg";
+
 const AboutPage = () => {
   return (
     <>
       <div className="aboutPage">
         <div className="img">
-          <img src="" alt="" />
+          <img src={topGarage1} alt="image" />
           <div className="imgInside">
-            <img src="" alt="" />
-            <img src="" alt="" />
+            <img className="first" src={topGarage2} alt="" />
+            <img className="second" src={topGarage3} alt="" />
           </div>
         </div>
         <div className="desc">
