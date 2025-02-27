18419a9cb6040b0bd41455953fa1305c6d850a39
Ardiantohendy <bayuardianto421@gmail.com>
2025-02-27 16:30:08 +0700

    new commit = restyling services page

--- src/components/ServicesPage.jsx
+++ src/components/ServicesPage.jsx
@@ -2,28 +2,41 @@ const ServicesPage = () => {
   return (
     <>
       <div className="servicePage">
-        <div className="top">
+        {/* <div className="top">
           <h2>Our Services</h2>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore, excepturi officia? Quis id repudiandae numquam?</p>
           <hr />
-        </div>
+        </div> */}
         <div className="bot">
           <div className="box1">
+            <h2>Our Services</h2>
             <h3>Check Our Services</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti deleniti tenetur nam molestias laborum obcaecati?</p>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti deleniti tenetur nam molestias laborum obcaecati? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore, excepturi officia? Quis id repudiandae
+              numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, minima!
+            </p>
             <p className="bold">All Services</p>
           </div>
-          <div className="box box2">
-            <p>Engine Upgrade</p>
-          </div>
-          <div className="box box3">
-            <p>Inspection Service</p>
-          </div>
-          <div className="box box4">
-            <p>Engine Repair</p>
-          </div>
-          <div className="box box5">
-            <p>Body Repair</p>
+          <div className="boxList">
+            <div className="box box2">
+              <p>Engine Upgrade</p>
+            </div>
+            <div className="box box3">
+              <p>Inspection Service</p>
+            </div>
+            <div className="box box4">
+              <p>Engine Repair</p>
+            </div>
+            <div className="box box5">
+              <p>Body Repair</p>
+            </div>
+            <div className="box box6">
+              <p>Electricity Repair</p>
+            </div>
+            <div className="box box7">
+              <p>Tire Repair</p>
+            </div>
           </div>
         </div>
       </div>
