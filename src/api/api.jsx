0ffcdae33d312d4bad8787c9c364273e792b48e4
Ardiantohendy <bayuardianto421@gmail.com>
2025-03-09 17:31:23 +0700

    new commit register user and connected it with database

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,6 +1,15 @@
-// import axios from "axios";
+import axios from "axios";
 
-// export const addBook = async () => {
-//     const booking = await axios.post("http://127.0.0.1:8000/add/")
-//     re
-// }
+const API_URL = "http://127.0.0.1:8000/";
+
+export const register = async (userData) => {
+  return await axios.post(API_URL + "register/", userData);
+};
+
+export const login = async (userData) => {
+  return await axios.post(API_URL + "login/", userData);
+};
+
+export const logout = async (refreshToken) => {
+  return await axios.post(API_URL, "logout/", { refresh: refreshToken });
+};
--- src/components/RegisterComponent.jsx
+++ src/components/RegisterComponent.jsx
@@ -1,29 +1,47 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { register } from "../api/api";
 
 const RegisterComponent = () => {
   const navigate = useNavigate();
+  const [user, setUser] = useState({ username: "", email: "", password: "", password2: "" });
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await register(user);
+      alert("Registration successful!");
+    } catch (error) {
+      console.log("Registration failed", error);
+    }
+  };
+
   return (
     <>
       <div className="register">
         <div className="container">
           <h2>Welcome To Doctor Car</h2>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam earum, atque facilis assumenda rem adipisci cumque dolores? Exercitationem, iste tempora?</p>
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <div className="username">
               <label>Username:</label>
-              <input type="text" name="username" /> {/* need value and onChange */}
+              <input type="text" name="username" placeholder="Username" onChange={handleChange} /> {/* need value and onChange */}
             </div>
             <div className="email">
               <label>Email:</label>
-              <input type="email" name="email" /> {/* need value and onChange */}
+              <input type="email" name="email" placeholder="Email" onChange={handleChange} /> {/* need value and onChange */}
             </div>
             <div className="password">
               <label>Password:</label>
-              <input type="password" name="password" /> {/* need value and onChange */}
+              <input type="password" name="password" placeholder="Password" onChange={handleChange} /> {/* need value and onChange */}
             </div>
             <div className="password">
               <label>Repeat Password:</label>
-              <input type="password" name="password2" /> {/* need value and onChange */}
+              <input type="password" name="password2" placeholder="Repeat Password" onChange={handleChange} /> {/* need value and onChange */}
             </div>
             <div className="button">
               <button type="submit">Sign Up</button>
