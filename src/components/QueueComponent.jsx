1b0e6177fbda214c0666a720d3693372f8b550d6
Ardiantohendy <bayuardianto421@gmail.com>
2025-03-11 00:22:51 +0700

    styling queue component

--- src/components/QueueComponent.jsx
+++ src/components/QueueComponent.jsx
@@ -7,16 +7,117 @@ const QueueComponent = () => {
             <table>
               <thead>
                 <tr>
+                  <th>No.</th>
                   <th>Brand</th>
                   <th>Model</th>
                   <th>Car Number</th>
                   <th>Date & Time</th>
-                  <th>Services</th>
-                  <th>Bill</th>
+                  <th className="service">Services</th>
+                  <th className="price">Bill</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
+                  <td>1</td>
+                  <td>Toyota</td>
+                  <td>Avanza</td>
+                  <td>F 1366 PT</td>
+                  <td>2025-03-14 & 07:10</td>
+                  <td>
+                    <ul>
+                      <li>
+                        <p>Engine Upgrade</p>
+                      </li>
+                      <li>
+                        <p>Engine Repair</p>
+                      </li>
+                      <li>
+                        <p>Body Repair</p>
+                      </li>
+                    </ul>
+                  </td>
+                  <td>
+                    <ul>
+                      <li>
+                        <p>Rp. 2.500.000,00</p>
+                      </li>
+                      <li>
+                        <p>Rp. 1.000.000,00</p>
+                      </li>
+                      <li>
+                        <p>Rp. 1.000.000,00</p>
+                      </li>
+                    </ul>
+                  </td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Toyota</td>
+                  <td>Avanza</td>
+                  <td>F 1366 PT</td>
+                  <td>2025-03-14 & 07:10</td>
+                  <td>
+                    <ul>
+                      <li>
+                        <p>Engine Upgrade</p>
+                      </li>
+                      <li>
+                        <p>Engine Repair</p>
+                      </li>
+                      <li>
+                        <p>Body Repair</p>
+                      </li>
+                    </ul>
+                  </td>
+                  <td>
+                    <ul>
+                      <li>
+                        <p>Rp. 2.500.000,00</p>
+                      </li>
+                      <li>
+                        <p>Rp. 1.000.000,00</p>
+                      </li>
+                      <li>
+                        <p>Rp. 1.000.000,00</p>
+                      </li>
+                    </ul>
+                  </td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Toyota</td>
+                  <td>Avanza</td>
+                  <td>F 1366 PT</td>
+                  <td>2025-03-14 & 07:10</td>
+                  <td>
+                    <ul>
+                      <li>
+                        <p>Engine Upgrade</p>
+                      </li>
+                      <li>
+                        <p>Engine Repair</p>
+                      </li>
+                      <li>
+                        <p>Body Repair</p>
+                      </li>
+                    </ul>
+                  </td>
+                  <td>
+                    <ul>
+                      <li>
+                        <p>Rp. 2.500.000,00</p>
+                      </li>
+                      <li>
+                        <p>Rp. 1.000.000,00</p>
+                      </li>
+                      <li>
+                        <p>Rp. 1.000.000,00</p>
+                      </li>
+                    </ul>
+                  </td>
+                </tr>
+                <tr>
+                  <td>4</td>
                   <td>Toyota</td>
                   <td>Avanza</td>
                   <td>F 1366 PT</td>
@@ -51,8 +152,15 @@ const QueueComponent = () => {
               </tbody>
               <tfoot>
                 <tr>
-                  <th>Total</th>
-                  <td>Rp. 4.500.000,00</td>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <td>
+                    <p>Rp. 4.500.000,00</p>
+                  </td>
                 </tr>
               </tfoot>
             </table>
