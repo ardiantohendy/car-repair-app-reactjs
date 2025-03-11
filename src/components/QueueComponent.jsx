e4f16a49d7ec60e104e43691189d5e11a4b14860
Ardiantohendy <bayuardianto421@gmail.com>
2025-03-11 14:53:12 +0700

    new commit (hiatus)

--- src/components/QueueComponent.jsx
+++ src/components/QueueComponent.jsx
@@ -1,5 +1,53 @@
 import Header from "./Header";
 
+const data = [
+  {
+    id: 1,
+    brand: "Toyota",
+    model: "Avanza",
+    plate: "F 1366 PT",
+    date: "2025-03-14 & 07:10",
+    services: ["Engine Upgrade", "Engine Repair", "Body Repair"],
+    prices: ["2.500.000,00", "1.000.000,00", "1.000.000,00"],
+  },
+  {
+    id: 2,
+    brand: "Honda",
+    model: "Civic",
+    plate: "B 2025 ZZ",
+    date: "2025-03-15 & 09:00",
+    services: ["Oil Change", "Brake Repair"],
+    prices: ["500.000,00", "750.000,00"],
+  },
+  {
+    id: 3,
+    brand: "Suzuki",
+    model: "Ertiga",
+    plate: "D 7777 AB",
+    date: "2025-03-16 & 10:30",
+    services: ["Engine Upgrade", "Body Repair"],
+    prices: ["2.000.000,00", "1.500.000,00"],
+  },
+  {
+    id: 4,
+    brand: "Mitsubishi",
+    model: "Pajero",
+    plate: "N 1234 KL",
+    date: "2025-03-17 & 08:45",
+    services: ["Transmission Repair"],
+    prices: ["3.000.000,00"],
+  },
+  {
+    id: 5,
+    brand: "Mitsubishi",
+    model: "Pajero",
+    plate: "N 1234 KT",
+    date: "2025-03-17 & 10:45",
+    services: ["Transmission Repair", "Engine Upgrade"],
+    prices: ["3.000.000,00", "2.000.000,00"],
+  },
+];
+
 const QueueComponent = () => {
   return (
     <>
@@ -26,39 +74,33 @@ const QueueComponent = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>1</td>
-                <td>Toyota</td>
-                <td>Avanza</td>
-                <td>F 1366 PT</td>
-                <td>2025-03-14 & 07:10</td>
-                <td>
-                  <ul>
-                    <li>
-                      <p>Engine Upgrade</p>
-                    </li>
-                    <li>
-                      <p>Engine Repair</p>
-                    </li>
-                    <li>
-                      <p>Body Repair</p>
-                    </li>
-                  </ul>
-                </td>
-                <td>
-                  <ul>
-                    <li>
-                      <p>Rp. 2.500.000,00</p>
-                    </li>
-                    <li>
-                      <p>Rp. 1.000.000,00</p>
-                    </li>
-                    <li>
-                      <p>Rp. 1.000.000,00</p>
-                    </li>
-                  </ul>
-                </td>
-              </tr>
+              {data.map((item, index) => (
+                <tr key={index}>
+                  <td>{(index += 1)}</td>
+                  <td>{item.brand}</td>
+                  <td>{item.model}</td>
+                  <td>{item.plate}</td>
+                  <td>{item.date}</td>
+                  <td>
+                    <ul>
+                      {item.services.map((service, i) => (
+                        <li key={i}>
+                          <p>{service}</p>
+                        </li>
+                      ))}
+                    </ul>
+                  </td>
+                  <td>
+                    <ul>
+                      {item.prices.map((price, i) => (
+                        <li key={i}>
+                          <p>Rp. {price}</p>
+                        </li>
+                      ))}
+                    </ul>
+                  </td>
+                </tr>
+              ))}
             </tbody>
             <tfoot>
               <tr>
